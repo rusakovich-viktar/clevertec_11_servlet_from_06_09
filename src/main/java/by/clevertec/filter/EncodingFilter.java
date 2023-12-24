b48cd7c7f0429a373d989085699372f746a2decd
Viktar Rusakovich <mrisviz.rus@gmail.com>
2023-12-25 01:17:22 +0300

    feat: encoding filter

---
+++ src/main/java/by/clevertec/filter/EncodingFilter.java
@@ -0,0 +1,37 @@
+package by.clevertec.filter;
+
+import jakarta.servlet.Filter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.FilterConfig;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.annotation.WebFilter;
+import jakarta.servlet.annotation.WebInitParam;
+import java.io.IOException;
+import lombok.extern.log4j.Log4j2;
+
+@Log4j2
+@WebFilter(urlPatterns = "/*",
+           initParams = {@WebInitParam(name = "encoding", value = "UTF-8", description = "Encoding params")})
+public class EncodingFilter implements Filter {
+
+    private String encoding;
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        encoding = filterConfig.getInitParameter("encoding");
+        log.info("encoding filter is init");
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        if (!encoding.equals(request.getCharacterEncoding())) {
+            request.setCharacterEncoding(encoding);
+            response.setCharacterEncoding(encoding);
+            response.setContentType("application/json");
+        }
+        chain.doFilter(request, response);
+    }
+
+}
